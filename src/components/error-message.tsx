6c7e60a2e2e4d4d35e3e46928561cfb19fa248d8
Bailig <barrybailig@hotmail.com>
2022-09-04 09:45:57 +0200

    feat: added global error handling

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,13 +1,15 @@
 export const ErrorMessage = ({
   error,
+  touched,
   className,
 }: {
+  touched?: boolean;
   error?: string;
   className?: string;
 }) => (
   <div
     className={`min-h-[24px] text-red-500 ${
-      error ? "opacity-100" : "opacity-0"
+      error && touched ? "opacity-100" : "opacity-0"
     } ${className}`}
   >
     {error}
